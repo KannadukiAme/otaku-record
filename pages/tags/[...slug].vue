e6548c171e6b2caba089d0f716ff104f13016fd6
KannadukiAme <misuzunagisa@gmail.com>
2023-11-07 11:12:51 +0800

    feat: tags page

--- pages/tags/[...slug].vue
+++ pages/tags/[...slug].vue
@@ -1,3 +1,36 @@
+<script setup>
+import { format } from 'date-fns'
+
+const route = useRoute()
+
+const tags = route.params.slug
+
+const { data: articles } = await useAsyncData('page', () =>
+  queryContent('blog')
+    .where({ tags: { $in: tags } })
+    .sort({ pubDate: -1 })
+    .find()
+)
+
+const count = articles.value.length
+</script>
+
 <template>
-  <div class="h-[600px]">正在开发中...</div>
+  <div class="max-w-screen-lg min-h-[600px] mx-auto mb-[80px] px-4">
+    <div class="text-2xl mb-2">
+      <Icon name="uil:tag-alt"></Icon>
+      <span class="ml-2">{{ `${tags[0]} (${count})` }}</span>
+    </div>
+    <template v-for="article in articles">
+      <a
+        class="mb-2 flex justify-between hover:text-sora"
+        :href="article._path"
+      >
+        <span>{{ article.title }}</span>
+        <span class="text-gray-500">{{
+          format(new Date(article.pubDate), 'yyyy-MM-dd')
+        }}</span>
+      </a>
+    </template>
+  </div>
 </template>
